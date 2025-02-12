caa39e22812d4c69e7210c9ad18cc2639c3a71c6
MarwanSayed014 <marwansayed014@gmail.com>
2025-02-12 21:40:33 +0200

    Refactoring

--- Chat/Dtos/PrivateMessageDto.cs
+++ Chat/Dtos/PrivateMessageDto.cs
@@ -9,8 +9,10 @@ namespace ChatAPI.Dtos
         [MinLength(1, ErrorMessage = "Message should be have at least 1 character")]
         [MaxLength(250, ErrorMessage = "Message Length should be less than or equal 250")]
         public string Text { get; set; }
+        
         [Required]
         public Guid AnotherUserId { get; set; }
+
         [MaxLength(10,ErrorMessage = "Number of files should be less than or equal 10")]
         [MaxFileSize(10_000_000, true, ErrorMessage = "The file should be less than 10 MB")]
         [AllowedExtensions([".png", ".jpg", ".jpeg"], true,
