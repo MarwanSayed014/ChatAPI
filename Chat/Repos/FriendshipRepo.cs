dd98aa5eadfb8ef8a2934670c53d5e0e76c08dcf
MarwanSayed014 <marwansayed014@gmail.com>
2025-02-16 22:36:12 +0200

    Refactoring

--- Chat/Repos/FriendshipRepo.cs
+++ Chat/Repos/FriendshipRepo.cs
@@ -10,7 +10,27 @@ namespace ChatAPI.Repos
         public FriendshipRepo(ApplicationDbContext context) : base(context)
         {
         }
+        public async Task<IEnumerable<Friendship>> GetFriendsAsync(Guid userId)
+        {
+            return (await FindAsync(x =>
+                        (x.RequestorUserId == userId || x.RespondentUserId == userId)
+                            && x.FriendshipStatus == FriendshipStatus.Friends)
+                    );
+        }
+        public async Task<IEnumerable<Friendship>> GetPendingFriendRequestsAsync(Guid userId)
+        {
+            return (await FindAsync(x =>
+                        x.RespondentUserId == userId
+                            && x.IsDelivered == false)
+                    );
+        }
 
-
+        public async Task<IEnumerable<Friendship>> GetRecentlyAcceptedRequestAsync(Guid userId)
+        {
+            return (await FindAsync(x =>
+                        x.RequestorUserId == userId
+                            && x.NotifyAcceptance == true)
+                    );
+        }
     }
 }
