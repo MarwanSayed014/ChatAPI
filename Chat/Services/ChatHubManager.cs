3d79cfbcbce389e684e88039197b1ba1787d6cbc
MarwanSayed014 <marwansayed014@gmail.com>
2025-02-19 23:53:41 +0200

    Refactoring

--- Chat/Services/ChatHubManager.cs
+++ Chat/Services/ChatHubManager.cs
@@ -1,4 +1,5 @@
 ﻿using ChatAPI.Dtos;
+using ChatAPI.Hubs;
 using ChatAPI.Models;
 using ChatAPI.Repos.Interfaces;
 using ChatAPI.Services.Interfaces;
@@ -11,22 +12,31 @@ namespace ChatAPI.Services
 {
     public class ChatHubManager : IChatHubManager
     {
-        public IChatService _chatService { get; }
+        public IPrivateChatService _privateChatService { get; }
         public IUserConnectionsManager _userConnectionsManager { get; }
         public IUserRepo _userRepo { get; }
+        public IFriendshipManager _friendshipManager { get; }
+        public IPrivateMessageManager _messageManager { get; }
 
-        public ChatHubManager(IChatService chatService, IUserConnectionsManager userConnectionsManager,
-            IUserRepo userRepo)
+        public Microsoft.AspNetCore.SignalR.IHubContext<ChatHub> _hubContext { get; set; }
+
+        public ChatHubManager(Microsoft.AspNetCore.SignalR.IHubContext<ChatHub> hubContext, IPrivateChatService privateChatService, IUserConnectionsManager userConnectionsManager,
+            IUserRepo userRepo, IFriendshipManager friendshipManager, IPrivateMessageManager messageManager)
         {
-            _chatService = chatService;
+            _hubContext = hubContext;
+            _privateChatService = privateChatService;
             _userConnectionsManager = userConnectionsManager;
             _userRepo = userRepo;
+            _friendshipManager = friendshipManager;
+            _messageManager = messageManager;
         }
-        public async Task OnConnectedAsync(IHubCallerClients clients, IGroupManager groups, 
-            Guid currentUserId, string connectionId)
+        public async Task OnConnectedAsync(Guid currentUserId, string connectionId)
         {
             string currentUserName = await _userRepo.GetUserName(currentUserId);
-            List<PrivateChat> userPrivateChats = _chatService.GetAllUserPrivateChatsAsync(currentUserId).WaitAndGetResult().ToList();
+            List<PrivateChat> userPrivateChats = _privateChatService.GetAllUserPrivateChatsAsync(currentUserId).WaitAndGetResult().ToList();
+            List<Friendship> friends = _friendshipManager.GetFriendsAsync(currentUserId).WaitAndGetResult().ToList();
+            List<Friendship> pendingFriendRequests = _friendshipManager.GetPendingFriendRequestsAsync(currentUserId).WaitAndGetResult().ToList();
+            List<Friendship> recentlyAcceptedRequests = _friendshipManager.GetRecentlyAcceptedRequestsAsync(currentUserId).WaitAndGetResult().ToList();
             bool wasOnline = _userConnectionsManager.IsOnline(currentUserId).WaitAndGetResult();
 
 
@@ -40,41 +50,60 @@ namespace ChatAPI.Services
             //Add To All Private Chats
             foreach (var item in userPrivateChats)
             {
-                groups.AddToGroupAsync(connectionId, item.Id.ToString()).Wait();
+                _hubContext.Groups.AddToGroupAsync(connectionId, item.Id.ToString()).Wait();
             }
 
             //add to all groups
 
 
 
+
             //Create Online Group And Add User To It And Notify
-            //TODO: Notify Only [Friends]
-            if (!wasOnline)
+            foreach (var item in friends)
             {
-                clients.AllExcept(connectionId).SendAsync("userstatus", currentUserName, "Online");
+                _hubContext.Groups.AddToGroupAsync(connectionId, item.FriendshipId.ToString()).Wait();
+                if (!wasOnline)
+                {
+                    _hubContext.Clients.Group(item.FriendshipId.ToString()).SendAsync("userstatus", currentUserName, "Online").Wait();
+                }
             }
 
+            foreach(var item in pendingFriendRequests)
+            {
+                _hubContext.Clients.Client(connectionId).SendAsync("FriendRequestsDelivered", item.FriendshipId, "You have a new friend Request").Wait();
+                _friendshipManager.RequestDeliveredAsync(item.FriendshipId).Wait();
+            }
+
+            foreach(var item in recentlyAcceptedRequests)
+            {
+                _hubContext.Clients.Client(connectionId).SendAsync("AcceptFriendShipRequest", item.RespondentUserId, $"{item.RespondentUserId} Accept your friend request").Wait();
+                _friendshipManager.AcceptanceNotifiedAsync(item.FriendshipId).Wait();
+            }
+            
+
             //Push Pending Private Messages
-            List<PrivateMessage> privateMessages = _chatService.GetPendingPrivateMessagesAsync(currentUserId).WaitAndGetResult().ToList();
+            List<PrivateMessage> privateMessages = _messageManager.GetPendingPrivateMessagesAsync(currentUserId).WaitAndGetResult().ToList();
             foreach (var PrivateMessage in privateMessages)
             {
-                _chatService.MessageDeliveredAsync(PrivateMessage.Id).Wait();
-                clients.Group(PrivateMessage.PrivateChatId.ToString()).SendAsync("privateMessageStatusChanged", $"{PrivateMessage.Id} is Delivered", PrivateMessage);
+                _messageManager.MessageDeliveredAsync(PrivateMessage.Id).Wait();
+                _hubContext.Clients.Group(PrivateMessage.PrivateChatId.ToString()).SendAsync("privateMessageStatusChanged", $"{PrivateMessage.Id} is Delivered", PrivateMessage).Wait();
             }
 
             //Pending Messages Delivered
         }
 
-        public async Task OnDisconnectedAsync(IHubCallerClients clients, IGroupManager groups, Guid currentUserId, string connectionId)
+        public async Task OnDisconnectedAsync(Guid currentUserId, string connectionId)
         {
             string currentUserName = await _userRepo.GetUserName(currentUserId);
-            List<PrivateChat> userPrivateChats = _chatService.GetAllUserPrivateChatsAsync(currentUserId).WaitAndGetResult().ToList();
+            List<PrivateChat> userPrivateChats = _privateChatService.GetAllUserPrivateChatsAsync(currentUserId).WaitAndGetResult().ToList();
             _userConnectionsManager.RemoveUserConnectionAsync(connectionId).Wait();
+            List<Friendship> friends = _friendshipManager.GetFriendsAsync(currentUserId).WaitAndGetResult().ToList();
+
 
             //Remove From All private Chats Group
             foreach (var item in userPrivateChats)
             {
-                groups.RemoveFromGroupAsync(connectionId, item.Id.ToString());
+                _hubContext.Groups.RemoveFromGroupAsync(connectionId, item.Id.ToString());
             }
 
             //Remove From All Groups
@@ -82,9 +111,13 @@ namespace ChatAPI.Services
             //Remove User From Online Group And Notify
             //TODO: Notify Only [Friends]
             bool stillOnline = _userConnectionsManager.IsOnline(currentUserId).WaitAndGetResult();
-            if (!stillOnline)
+            foreach (var item in friends)
             {
-                clients.All.SendAsync("userstatus", currentUserName, "Offline");
+                _hubContext.Groups.RemoveFromGroupAsync(connectionId, item.FriendshipId.ToString()).Wait();
+                if (!stillOnline)
+                {
+                    _hubContext.Clients.Group(item.FriendshipId.ToString()).SendAsync("userstatus", currentUserName, "Offline");
+                }
             }
         }
     }
