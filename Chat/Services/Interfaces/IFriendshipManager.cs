3c97b6cc3bdaa10663493f5cf7450d568ffbdd17
MarwanSayed014 <marwansayed014@gmail.com>
2025-02-21 16:39:42 +0200

    Refactoring

--- Chat/Services/Interfaces/IFriendshipManager.cs
+++ Chat/Services/Interfaces/IFriendshipManager.cs
@@ -1,4 +1,5 @@
 ﻿using ChatAPI.Hubs;
+using ChatAPI.Models;
 using Microsoft.AspNetCore.SignalR;
 
 namespace ChatAPI.Services.Interfaces
@@ -7,5 +8,10 @@ namespace ChatAPI.Services.Interfaces
     {
         public Task<bool> SendFriendRequestAsync(Microsoft.AspNetCore.SignalR.IHubContext<ChatHub> hubContext, Guid requestorUserId, Guid respondentUserId);
         public Task<bool> AcceptFriendRequestAsync(Microsoft.AspNetCore.SignalR.IHubContext<ChatHub> hubContext, Guid currentUserId, Guid friendshipId);
+        public Task<IEnumerable<Friendship>> GetFriendsAsync(Guid userId);
+        public Task<IEnumerable<Friendship>> GetPendingFriendRequestsAsync(Guid userId);
+        public Task<IEnumerable<Friendship>> GetRecentlyAcceptedRequestsAsync(Guid currentUserId);
+        public Task RequestDeliveredAsync(Guid friendshipId);
+        public Task AcceptanceNotifiedAsync(Guid friendshipId);
     }
 }
