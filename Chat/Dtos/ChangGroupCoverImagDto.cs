adb29a3705922e5c5d66e5a06a3a933e91a4a68c
MarwanSayed014 <marwansayed014@gmail.com>
2025-02-28 01:06:47 +0200

    Refactoring

---
+++ Chat/Dtos/ChangGroupCoverImagDto.cs
@@ -0,0 +1,17 @@
+﻿using ChatAPI.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChatAPI.Dtos
+{
+    public class ChangGroupCoverImagDto
+    {
+        [Required]
+        public Guid GroupId { get; set; }
+
+        [Required]
+        [MaxFileSize(10_000_000, true, ErrorMessage = "The file should be less than 10 MB")]
+        [AllowedExtensions([".png", ".jpg", ".jpeg"], true,
+            ErrorMessage = "The file should be in this extensions (png, jpg, jpeg)")]
+        public IFormFile Image { get; set; }
+    }
+}
