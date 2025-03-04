38c3c4fcac3dda6bd446f6e8e73930dffb8fdc42
MarwanSayed014 <marwansayed014@gmail.com>
2025-03-05 00:08:51 +0200

    Add Private Chat Service

---
+++ Chat/Services/Interfaces/IPrivateChatService.cs
@@ -0,0 +1,12 @@
+﻿using ChatAPI.Dtos;
+using ChatAPI.Models;
+
+namespace ChatAPI.Services.Interfaces
+{
+    public interface IPrivateChatService
+    {
+        Task<IEnumerable<PrivateChat>> GetAllUserPrivateChatsAsync(Guid userId);
+        Task<PrivateChat> GetPrivateChatAsync(Guid currentUserId, Guid anotherUserId);
+        Task CreatePrivateChat(PrivateChat privateChat, Guid currentUserId, Guid anotherUserId);
+    }
+}
---
+++ Chat/Services/PrivateChatService.cs
@@ -0,0 +1,55 @@
+﻿using ChatAPI.Dtos;
+using ChatAPI.Helpers;
+using ChatAPI.Models;
+using ChatAPI.Repos.Interfaces;
+using ChatAPI.Services.Interfaces;
+using ChatAPI.Types;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ChatAPI.Services
+{
+    public class PrivateChatService : IPrivateChatService
+    {
+        public IPrivateChatRepo _privateChatRepo { get; }
+        public IUserPrivateChatRepo _userPrivateChatRepo { get; }
+        public IPrivateMessageRepo _privateMessageRepo { get; }
+        public IUserRepo _userRepo { get; }
+
+        public PrivateChatService(IPrivateChatRepo chatRepo, IUserPrivateChatRepo userPrivateChatRepo,
+            IPrivateMessageRepo messageRepo, IUserRepo userRepo)
+        {
+            _privateChatRepo = chatRepo;
+            _userPrivateChatRepo = userPrivateChatRepo;
+            _privateMessageRepo = messageRepo;
+            _userRepo = userRepo;
+        }
+
+
+
+        public async Task<IEnumerable<PrivateChat>> GetAllUserPrivateChatsAsync(Guid userId)
+        {
+            return (await _userPrivateChatRepo.GetAllUserPrivateChatsAsync(userId)).ToList();
+        }
+
+        public async Task<PrivateChat> GetPrivateChatAsync(Guid currentUserId, Guid anotherUserId)
+        {
+            return await _userPrivateChatRepo.GetPrivateChatAsync(currentUserId, anotherUserId);
+        }
+
+        public async Task CreatePrivateChat(PrivateChat privateChat, Guid currentUserId, Guid anotherUserId)
+        {
+            await _privateChatRepo.CreateAsync(privateChat);
+            await _privateChatRepo.SaveAsync();
+
+            await _userPrivateChatRepo.AddUsersToPrivateChat(
+                new List<Guid>
+                {
+                        currentUserId,
+                        anotherUserId
+                },
+                privateChat.Id);
+            await _userPrivateChatRepo.SaveAsync();
+        }
+    }
+}
